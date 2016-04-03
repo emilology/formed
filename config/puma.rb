ffb7a5b1595b6363d5d7dc81cdee7387261fec8d
ES <emsta24@gmail.com>
2016-04-03 16:22:17 +1200

    changing puma

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,6 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
   ActiveRecord::Base.establish_connection
 end
