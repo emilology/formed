5c6c30fa8b7a8b95e9b69456f420e0cda5a29edc
ES <emsta24@gmail.com>
2016-04-03 14:31:51 +1200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
-  get 'products/:id', to: 'products#show', :as => :products
+  root to: 'home#index'
   devise_for :users
   resources :users
 end
