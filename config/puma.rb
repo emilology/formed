5091a5c689166d7ed0d7e305fbb3b195a381225c
ES <emsta24@gmail.com>
2016-04-03 16:23:17 +1200

    puma v2

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@ preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 
 on_worker_boot do
   # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
